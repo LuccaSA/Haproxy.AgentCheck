795bd66459b0ae70054e8dac2a6a0be0bec01222
Raphaël DUCOM <raphael.ducom@gmail.com>
2020-04-27 21:04:29 +0200

    limit weight with option limit

--- HostProbe/RawTcpMiddleware.cs
+++ HostProbe/RawTcpMiddleware.cs
@@ -2,16 +2,19 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.Options;
 
 namespace HostProbe
 {
     public class RawTcpMiddleware : ConnectionHandler
     {
         private readonly State _state;
+        private readonly IOptionsMonitor<HostProbeConfig> _options;
 
-        public RawTcpMiddleware(State state)
+        public RawTcpMiddleware(State state, IOptionsMonitor<HostProbeConfig> options)
         {
             _state = state;
+            _options = options;
         }
 
         public override async Task OnConnectedAsync(ConnectionContext connection)
@@ -22,10 +25,14 @@ namespace HostProbe
             }
             async Task ReportUsageTcpAsync()
             {
-                await connection.Transport.Output.WriteAsync(AsReadOnlyMemory("UP%\n"));
-                await connection.Transport.Output.WriteAsync(AsReadOnlyMemory("ready\n"));
-                await connection.Transport.Output.WriteAsync(AsReadOnlyMemory($"{100-_state.CpuPercent}%\n"));
-                await connection.Transport.Output.WriteAsync(AsReadOnlyMemory("\n"));
+                int max = _options.CurrentValue.CpuLimit;
+                int weight = (int)((max - _state.CpuPercent) / (double)max * 100);
+                if (weight == 0)
+                {
+                    // security to avoid full drain mode
+                    weight = 1;
+                }
+                await connection.Transport.Output.WriteAsync(AsReadOnlyMemory($"up {weight}%\n"));
                 await connection.Transport.Output.FlushAsync();
             }
 
