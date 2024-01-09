bd6e4ad4441884dd3720b410d261b7b057e927de
Nathanael Marchand <NatMarchand@users.noreply.github.com>
2024-01-09 15:42:00 +0100

    chore: change log message

--- Haproxy.AgentCheck/Hosting/MonitoringSession.Logs.cs
+++ Haproxy.AgentCheck/Hosting/MonitoringSession.Logs.cs
@@ -3,14 +3,14 @@
 #pragma warning disable S3881
 internal partial class MonitoringSession
 {
-    [LoggerMessage(LogLevel.Error, "No process {processName} currently running.")]
-    public static partial void LogNoProcessRunning(ILogger logger, string processName);
+    [LoggerMessage(LogLevel.Error, "No process {watchedProcess} currently running.")]
+    public static partial void LogNoProcessRunning(ILogger logger, string watchedProcess);
 
-    [LoggerMessage(LogLevel.Warning, "Found {count} {processName} currently running, watching pid {pid}.")]
-    public static partial void LogMoreThanOneProcessRunning(ILogger logger, string processName, int pid, int count);
+    [LoggerMessage(LogLevel.Warning, "Found {count} {watchedProcess} currently running, watching pid {pid}.")]
+    public static partial void LogMoreThanOneProcessRunning(ILogger logger, string watchedProcess, int pid, int count);
 
-    [LoggerMessage(LogLevel.Information, "Watching {processName} pid {pid}.")]
-    public static partial void LogOneProcessRunning(ILogger logger, string processName, int pid);
+    [LoggerMessage(LogLevel.Information, "Watching {watchedProcess} pid {pid}.")]
+    public static partial void LogOneProcessRunning(ILogger logger, string watchedProcess, int pid);
 
     [LoggerMessage(LogLevel.Warning, "An error occured while watching process. Process may have ended, retrying.")]
     public static partial void LogProcessingException(ILogger logger, Exception e);
